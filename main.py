f778c08af9b0eb6476eb61735c0b911f091b3869
Akhil Nedunuri <akhilnedunuri7@gmail.com>
2026-01-12 15:14:16 +0530

    updated the code with farmos get requests

--- main.py
+++ main.py
@@ -3,8 +3,16 @@ import requests
 
 mcp = FastMCP("My MCP Server")
 
+
 @mcp.tool
-def weather(city) -> str:
-    url = f"https://wttr.in/{city}?format=j1"
+def health():
+    url = "https://farmosapi.graylogic.com/health"
     data = requests.get(url).json()
-    return str(data["current_condition"][0]["temp_C"])
+    return data
+
+
+@mcp.tool
+def data_types():
+    url = "https://farmosapi.graylogic.com/data-types"
+    data = requests.get(url).json()
+    return data
